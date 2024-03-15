16f3a68ab50d3437555f9aa471a0b32a460ca336
G-Tech-007 <156540256+G-Tech-007@users.noreply.github.com>
2024-03-16 00:23:48 +0530

    GSP281_Introduction_to_SQL_for_BigQuery_and_Cloud_SQL.sh

---
+++ Introduction to SQL for BigQuery and Cloud SQL/GSP281_Introduction_to_SQL_for_BigQuery_and_Cloud_SQL.sh
@@ -0,0 +1,19 @@
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID
+
+
+curl -O https://raw.githubusercontent.com/G-Tech-007/LAB-SOLUTIONS/main/Introduction%20to%20SQL%20for%20BigQuery%20and%20Cloud%20SQL/start_station_name.csv
+curl -O https://raw.githubusercontent.com/G-Tech-007/LAB-SOLUTIONS/main/Introduction%20to%20SQL%20for%20BigQuery%20and%20Cloud%20SQL/end_station_name.csv
+
+gsutil cp start_station_name.csv gs://$DEVSHELL_PROJECT_ID/
+gsutil cp end_station_name.csv gs://$DEVSHELL_PROJECT_ID/
+
+
+
+gcloud sql instances create my-demo \
+    --database-version=MYSQL_5_7 \
+    --region=$REGION \
+    --tier=db-f1-micro \
+    --root-password=gtech
+
+gcloud sql databases create bike --instance=my-demo
