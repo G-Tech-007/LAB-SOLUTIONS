d386dab502ad2ae1b19e605f296af2af623c7d12
G-Tech-007 <156540256+G-Tech-007@users.noreply.github.com>
2024-04-14 00:38:39 +0530

    Create ARC130_Analyze_Sentiment_with_Natural_Language_API_Challenge_Lab.sh

---
+++ Analyze Sentiment with Natural Language API: Challenge Lab/ARC130_Analyze_Sentiment_with_Natural_Language_API_Challenge_Lab.sh
@@ -0,0 +1,43 @@
+
+gcloud services enable apikeys.googleapis.com
+gcloud alpha services api-keys create --display-name="GTECH" 
+KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=GTECH")
+API_KEY=$(gcloud alpha services api-keys get-key-string $KEY_NAME --format="value(keyString)")
+
+cat > analyze-request.json <<EOF
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content": "Google, headquartered in Mountain View, unveiled the new Android phone at the Consumer Electronic Show.  Sundar Pichai said in his keynote that users love their new Android phones."
+  },
+  "encodingType": "UTF8"
+}
+EOF
+
+curl -s -H "Content-Type: application/json" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+"https://language.googleapis.com/v1/documents:analyzeSyntax" \
+-d @analyze-request.json > analyze-response.txt
+
+
+
+
+
+cat > multi-nl-request.json <<EOF
+{
+  "document": {
+    "type": "PLAIN_TEXT",
+    "content": "Le bureau japonais de Google est situé à Roppongi Hills, Tokyo.",
+    "language": "fr"
+  }
+}
+
+EOF
+
+
+curl -s -H "Content-Type: application/json" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+"https://language.googleapis.com/v1/documents:analyzeEntities" \
+-d @multi-nl-request.json > multi-response.txt
+
+
